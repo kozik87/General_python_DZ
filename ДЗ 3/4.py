cb538700ed3319f7aba1333bfd4891e654cbee31
Kozik87 <konstantin.karnaukhov@gmail.com>
2021-09-21 00:06:09 +0300

    Change DZ3.4 a bit

--- ДЗ 3/4.py
+++ ДЗ 3/4.py
@@ -14,24 +14,42 @@
 # }
 # Как поступить, если потребуется сортировка по ключам?
 
+# def thesaurus_adv(*args):
+#     """ 
+#     This function is creating dictionary with keys - first letter of surname 
+#     and arguments are dictionary with keys - first letter of firstname
+#     """
+#     my_dict = {}
+#     my_dict_names = {}
+
+#     for i in args:
+#         if not i.split(" ")[1][0] in my_dict: 
+#             my_dict.setdefault(i.split(" ")[1][0], {}).setdefault(i[0], [i])
+#         else:         
+#             if i[0] in my_dict[i.split(" ")[1][0]]:
+#                 my_dict_names = my_dict.get(i.split(" ")[1][0])
+#                 my_dict[i.split(" ")[1][0]][i[0]]= my_dict_names.get(i[0]) + [i]    
+#             else:
+#                 my_dict.setdefault(i.split(" ")[1][0], {}).setdefault(i[0], [i])
+#     return my_dict
+
+
+# print(thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева"))
+
+#Вариант 2
 def thesaurus_adv(*args):
     """ 
     This function is creating dictionary with keys - first letter of surname 
     and arguments are dictionary with keys - first letter of firstname
     """
     my_dict = {}
-    my_dict_names = {}
 
-    for i in args:
-        if not i.split(" ")[1][0] in my_dict: 
-            my_dict.setdefault(i.split(" ")[1][0], {}).setdefault(i[0], [i])
-        else:         
-            if i[0] in my_dict[i.split(" ")[1][0]]:
-                my_dict_names = my_dict.get(i.split(" ")[1][0])
-                my_dict[i.split(" ")[1][0]][i[0]]= my_dict_names.get(i[0]) + [i]    
-            else:
-                my_dict.setdefault(i.split(" ")[1][0], {}).setdefault(i[0], [i])
+    for fullname in args:
+        
+        firstname, surname = fullname.split()
+
+        my_dict.setdefault(surname[0], {}).setdefault(firstname[0], []).append(fullname)
+
     return my_dict
-
 
 print(thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева"))
